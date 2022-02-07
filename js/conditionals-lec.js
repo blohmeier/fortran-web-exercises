5c0898b2daf2cf479ec5b1f6c40d7ac69731659a
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-07 11:24:28 -0600

    adds 'Mini-Exercise' work

--- js/conditionals-lec.js
+++ js/conditionals-lec.js
@@ -9,23 +9,23 @@
 //     console.log("You're an admin")
 // }
 
-var itemPrice = 4.99;
-var moneyInMyWallet = 10.00;
-
-if (moneyInMyWallet > itemPrice) {
-    console.log("You can buy this item")
-} else {
-    console.log("You can not buy this item")
-}
-
-var age = 21;
-var priceOfDrink = 8.00;
-
-if (age >= 21 && moneyInMyWallet >= priceOfDrink) {
-    console.log("You can buy a drink")
-} else {
-    console.log("nope")
-}
+// var itemPrice = 4.99;
+// var moneyInMyWallet = 10.00;
+//
+// if (moneyInMyWallet > itemPrice) {
+//     console.log("You can buy this item")
+// } else {
+//     console.log("You can not buy this item")
+// }
+//
+// var age = 21;
+// var priceOfDrink = 8.00;
+//
+// if (age >= 21 && moneyInMyWallet >= priceOfDrink) {
+//     console.log("You can buy a drink")
+// } else {
+//     console.log("nope")
+// }
 
 // MINI-EXERCISE
 // IF / ELSE IF / ELSE
@@ -39,9 +39,37 @@ if (age >= 21 && moneyInMyWallet >= priceOfDrink) {
 // If the rider is not over 16, has no insurance, OR has no car but they CAN get a rideshare, then they call up a rideshare service
 // If the rider meets none of the above, they should call a friend for a ride!
 //     This getToDestination should not return any value, but rather console log based on the conditions met.
-//     getToDestination, part 2
+// var age = 14;
+// var isInsured = false;
+// var hasCar = false;
+// var canGetRideShare = false;
+function getToDestination(age, isInsured, hasCar, canGetRideShare) {
+    if (age >= 16 && isInsured && hasCar) {
+        console.log("You can use your own vehicle")
+    } else if (age < 16 || !isInsured || !hasCar) {
+        if (canGetRideShare) {
+            console.log("Call up a rideshare service")
+        } else {
+            console.log("Call a friend for a ride")
+        }
+    }
+}
+getToDestination(16, true, true, true);
+// getToDestination, part 2
 // Create a function called canGetRideshare() which will RETURN a boolean
 // It will determine if there are drivers nearby and the person has enough money for the ride
 // Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
 // Call canGetRideshare() in the else if after !isLegalDriver &&...
 // Be sure to log to the user if they can or cannot get a rideshare
+// var driversNearby = true;
+// var hasEnoughMoney = false;
+// var hasCar = false;
+// var canGetRideShare = false;
+//
+// if (driversNearby && hasEnoughMoney && hasCar) {
+//     console.log("You can use your own vehicle")
+// } else if ((age < 16 || !isInsured || !hasCar) && canGetRideShare) {
+//     console.log("Call up a rideshare service")
+// } else {
+//     console.log("Call a friend for a ride")
+// }
