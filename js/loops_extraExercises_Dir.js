6fe48d8a2ffeafcaaf0d4a891e47d442c85fa946
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-11 07:28:32 -0600

    corrects prime number checker from loops_extraExercises.md

--- js/loops_extraExercises_Dir.js
+++ js/loops_extraExercises_Dir.js
@@ -12,17 +12,73 @@
 /* -->
 // Write the code necessary to output the first 50 prime numbers // Recommend starting your loop at 1 and ending your loop once you've calculated 50 primes. // https://en.wikipedia.org/wiki/Prime_number
  */
-var k = 1;
-for (let i = 1; i <= 230; i++) {
-    let isPrime = 0;
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) {
-            isPrime = 1;
-            break;
-        }
-    }
-    if (i > 1 && isPrime == 0) {
+//Attempt 1: Not working (incorrectly says 1 is prime).
+// for (let i = 1, k = 1; k <= 50; i++) {
+//     let isPrime = 0;
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) {
+//             isPrime = 1;
+//             break;
+//         }
+//     }
+//     if (isPrime == 0) {
+//         console.log("Prime number " + k + " is " + i);
+//         k++;
+//     }
+// }
+// Attempt 2: works for all values.
+for (let i = 1, k = 1; k <= 50; i++) {
+    if (test_prime(i)) {
         console.log("Prime number " + k + " is " + i);
         k++;
     }
+
 }
+function test_prime(n)
+{
+    if (n===1) {
+        return false;
+    }
+    else if (n === 2) {
+        return true;
+    } else {
+        for(let x = 2; x < n; x++) {
+            if(n % x === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//console.log(test_prime(37));
+// console.log(roughSizeOfObject(k));
+// function roughSizeOfObject( object ) {
+//
+//     var objectList = [];
+//     var stack = [object];
+//     var bytes = 0;
+//
+//     while (stack.length) {
+//         var value = stack.pop();
+//
+//         if (typeof value === 'boolean') {
+//             bytes += 4;
+//         } else if (typeof value === 'string') {
+//             bytes += value.length * 2;
+//         } else if (typeof value === 'number') {
+//             bytes += 8;
+//         } else if
+//         (
+//             typeof value === 'object'
+//             && objectList.indexOf(value) === -1
+//         ) {
+//             objectList.push(value);
+//
+//             for (var i in value) {
+//                 stack.push(value[i]);
+//             }
+//         }
+//         return bytes;
+//     }
+// }
