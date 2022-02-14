ffa9b3ae403e3e66a991c5856d9eb34c0b99405f
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-02-14 09:20:35 -0600

    updates  function with second effort

--- code_challenges/findAverage.js
+++ code_challenges/findAverage.js
@@ -14,9 +14,13 @@
 "use strict";
 function findAverage(grades) {
     let sum = 0;
-    grades.forEach(function(grade) {
-        sum += grade;
-    })
+    // grades.forEach(function(grade) {
+    //     sum += grade;
+    // })
+    // option 2 - for i
+    for (let i = 0; i < grades.length; i++) {
+        sum += grades[i];
+    }
     return sum / grades.length
 }
 console.log(findAverage([95, 74, 86, 100]));
